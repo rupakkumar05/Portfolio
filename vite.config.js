390f5e873c0ccc0650aa9f238c94f6651eb9445c
rupakkumar05 <rupakkumar995554@gmail.com>
2025-04-11 18:51:53 +0530

    package change

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   css: {
-    moduls: {
+    modules: {
       localsConvention: "camelCase",
     },
   },
-  base:"Portfolio",
+  base: "/Portfolio/",
 });
