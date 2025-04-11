67ba6caf9d4b2fd00696a3884cc9f6ae3c8addb4
rupakkumar05 <rupakkumar995554@gmail.com>
2025-04-11 19:38:12 +0530

    package change

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import styles from "./About.module.css";
 import hero from "../../assets/hero/rupak.png";
+import cursorIcon from "../../assets/about/cursorIcon.png";
+import serverIcon from "../../assets/about/serverIcon.png";
 
 const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-      <img
-        src={hero}
-        alt=" me sitting with a laptop" className={styles.aboutImage}
-      />
-      <ul className={styles.aboutItems}>
-        <li className={styles.aboutItem}>
-          <img src="./src/assets/about/cursorIcon.png" alt="cursor icon" />
-          <div className={styles.aboutItemText}>
-            <h3>Frontend Devloper</h3>
-            <p>
-              I'm a frontend developer with building responsive websitesand
-              optimise sites
-            </p>
-          </div>
-        </li>
+        <img
+          src={hero}
+          alt="me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={cursorIcon} alt="cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive websites and optimizing sites.
+              </p>
+            </div>
+          </li>
 
-        <li className={styles.aboutItem}>
-          <img src="./src/assets/about/serverIcon.png" alt="server icon" />
-          <div className={styles.aboutItemText}>
-            <h3>Backend Devloper</h3>
-            <p>
-              I'm a backend developer with devloping fast and optimised back-end
-              system with the Apis
-            </p>
-          </div>
-        </li>
-      </ul>
+          <li className={styles.aboutItem}>
+            <img src={serverIcon} alt="server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I'm a backend developer focused on developing fast and optimized back-end systems and APIs.
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
